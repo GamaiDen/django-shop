8deb96d779320d3e7751b8ab14cc0da5b5160e68
Даниил Тарлыгин <bambolbi05@gmail.com>
2026-04-28 17:16:16 +0300

    feat: Django forms with validation, CRUD for products, image validation

--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,63 @@
-from django.shortcuts import render
-def home(request):
-    return render(request, 'catalog/home.html')
-def contacts(request):
-    success = False
-    if request.method == 'POST':
-        success = True
-    return render(request, 'catalog/contacts.html', {'success': success})
+from django.shortcuts import get_object_or_404, redirect
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView, TemplateView
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from .models import Product
+from .forms import ProductForm
+
+
+class HomeView(ListView):
+    """Главная страница со списком товаров."""
+    model = Product
+    template_name = 'catalog/home.html'
+    context_object_name = 'products'
+
+
+class ProductDetailView(DetailView):
+    """Детальная страница товара."""
+    model = Product
+    template_name = 'catalog/product_detail.html'
+    context_object_name = 'product'
+
+
+class ProductCreateView(CreateView):
+    """Создание продукта."""
+    model = Product
+    form_class = ProductForm
+    template_name = 'catalog/product_form.html'
+    success_url = reverse_lazy('home')
+
+
+class ProductUpdateView(UpdateView):
+    """Редактирование продукта."""
+    model = Product
+    form_class = ProductForm
+    template_name = 'catalog/product_form.html'
+
+    def get_success_url(self):
+        return reverse_lazy('product_detail', kwargs={'pk': self.object.pk})
+
+
+class ProductDeleteView(DeleteView):
+    """Удаление продукта."""
+    model = Product
+    template_name = 'catalog/product_confirm_delete.html'
+    success_url = reverse_lazy('home')
+
+
+class ContactsView(TemplateView):
+    """Страница контактов."""
+    template_name = 'catalog/contacts.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['success'] = self.request.GET.get('success', False)
+        return context
+
+    def post(self, request, *args, **kwargs):
+        name = request.POST.get('name', '')
+        email = request.POST.get('email', '')
+        message = request.POST.get('message', '')
+        print(f"📨 {name} ({email}): {message}")
+        return HttpResponseRedirect(f"{reverse('contacts')}?success=1")
